198d0dafc8e310b8dcff062520a39b73db1a81ed
FABIAN MARTINEZ MARTELO <fabianmm04@gmail.com>
2020-02-02 21:03:44 -0500

    ai

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+declare var webkitSpeechRecognition: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  @ViewChild('search', {static:true}) busqueda: ElementRef;
   constructor() { }
 
   ngOnInit() {
+    const vSearch = new webkitSpeechRecognition();
+      vSearch.lang = "es";
+      vSearch.start();
+      const b = this.busqueda.nativeElement;
+      vSearch.onresult = function(event) {
+        // console.log(event);
+        b.value = event.results[0][0].transcript;
+    }
+  }
+
+  prueba(){
+    
+
   }
 
   loginForm = new FormGroup({
