5a43b9f3c150f38db544bb2469a147b927eb889c
FABIAN MARTINEZ MARTELO <fabianmm04@gmail.com>
2020-02-03 00:12:15 -0500

    habla

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { async } from '@angular/core/testing';
 declare var webkitSpeechRecognition: any;
 @Component({
   selector: 'app-login',
@@ -8,26 +9,41 @@ declare var webkitSpeechRecognition: any;
 })
 export class LoginComponent implements OnInit {
   @ViewChild('search', {static:true}) busqueda: ElementRef;
+  @ViewChild('hackhabla', {static:true}) habla: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    
+    
+    prueba("Hola soy tu asistente para llenar los formularios, Ingrese usuario a registrar");
     const vSearch = new webkitSpeechRecognition();
       vSearch.lang = "es";
       vSearch.start();
       const b = this.busqueda.nativeElement;
       vSearch.onresult = function(event) {
         // console.log(event);
-        b.value = event.results[0][0].transcript;
+        const transcript =  event.results[0][0].transcript;
+        b.value = transcript;
+        
     }
   }
 
-  prueba(){
-    
-
-  }
+  
 
   loginForm = new FormGroup({
     userName: new FormControl(''),
   });
 
+}
+
+ function prueba(message){
+  console.log("mensaje")
+
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  speechSynthesis.speak(speech)
 }
